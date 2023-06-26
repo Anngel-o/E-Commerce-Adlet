9f2ad214a66397a3661b27cd0430de958d8cd3aa
Anngel-o <anngelo.omz@gmail.com>
2023-06-25 23:20:54 -0600

    Mostrando productos con javascript

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const productos = [
     {
         id: "bolsa-beso-rosa",
         titulo: "Bolsa Beso Rosa",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -15,7 +15,7 @@ const productos = [
     {
         id: "bolsa-lila",
         titulo: "Bolsa Lila",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -25,7 +25,7 @@ const productos = [
     {
         id: "bolsa-lila-pequena",
         titulo: "Bolsa Lila Pequeña",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -35,7 +35,7 @@ const productos = [
     {
         id: "bolsa-azul-rey",
         titulo: "Bolsa Azul Rey",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -45,7 +45,7 @@ const productos = [
     {
         id: "bolsa-verde",
         titulo: "Bolsa Verde",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -55,7 +55,7 @@ const productos = [
     {
         id: "bolsa-olivo",
         titulo: "Bolsa Olivo",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -65,7 +65,7 @@ const productos = [
     {
         id: "bolsa-roja",
         titulo: "Bolsa Roja",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-azul-rey.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -75,7 +75,7 @@ const productos = [
     {
         id: "bolsa-buganbilia",
         titulo: "Bolsa Buganbilia",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -85,7 +85,7 @@ const productos = [
     {
         id: "bolsa-negra",
         titulo: "Bolsa Negra",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -95,7 +95,7 @@ const productos = [
     {
         id: "bolsa-negra-mono",
         titulo: "Bolsa Negra Moño",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -105,7 +105,7 @@ const productos = [
     {
         id: "bolsa-tommy",
         titulo: "Bolsa Tommy",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
             nombre: "Bolsas",
             id: "bolsas"
@@ -115,21 +115,62 @@ const productos = [
     {
         id: "bolsa-olivo",
         titulo: "Bolsa Olivo",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
-            nombre: "Bolsas",
-            id: "bolsas"
+            nombre: "Accesorios",
+            id: "accesorios"
         },
         precio: precioBolsa
     },
     {
         id: "bolsa-camel",
         titulo: "Bolsa Camel",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "./img/bolsa-tommy.jpg",
         categoria: {
-            nombre: "Bolsas",
-            id: "bolsas"
+            nombre: "Zapatos",
+            id: "zapatos"
         },
         precio: precioBolsa
     },
 ];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".button-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+
+function loadProducts(productosElegidos) {
+    contenedorProductos.innerHTML = ``;
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <center><img class="producto-imagen" src="${producto.imagen}" alt=${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio"> $${producto.precio} MX</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div></center>`;
+            contenedorProductos.append(div);
+    });  
+}
+
+loadProducts(productos);
+
+botonesCategorias.forEach(button => {
+    button.addEventListener("click", (event) => {
+        botonesCategorias.forEach(button => button.classList.remove("active"));
+        event.currentTarget.classList.add("active");
+
+        if (event.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id == event.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosButton = productos.filter(producto => producto.categoria.id === event.currentTarget.id);
+            loadProducts(productosButton);   
+        } else {
+            tituloPrincipal.innerText = "Todos los Productos";
+            loadProducts(productos);   
+        }
+    });
+});
